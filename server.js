80b2fa8ad35b7c5be1f06a1ca900242d37ffa744
irinamihai150@gmail.com <irinamihai50@gmail.com>
2023-01-02 11:56:33 +0000

    fix

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const moment = require("moment");
+moment("12-25-1995", "MM-DD-YYYY");
+
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const cors = require("cors");
+const { request } = require("express");
 
 const app = express();
 app.use(express.json());
@@ -20,6 +24,7 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
 app.post("/messages", (req, res) => {
   //validate our input
   if (!req.body.from || !req.body.text) {
@@ -28,7 +33,13 @@ app.post("/messages", (req, res) => {
   }
 
   // build our new resourse
-  const newMessage = req.body;
+  const newMessage = {
+    id: 3,
+    from: req.body.from,
+    text: req.body.text,
+    timestamp: moment(),
+  };
+  // const newMessage = req.body;
   newMessage.id = uuidv4();
   messages.push(newMessage);
   res.status(201).json(newMessage);
@@ -38,7 +49,6 @@ app.get("/messages", (req, res) => {
 });
 
 app.get("/messages/:id", (req, res) => {
-  console.log(req.params);
   const id = req.params.id;
   const msg = messages.find((message) => message.id === id);
   if (msg === undefined) {
@@ -47,9 +57,8 @@ app.get("/messages/:id", (req, res) => {
 
   res.send(msg);
 });
+
 app.delete("/messages/:id", (req, res) => {
-  // validate your input
-  const id = req.params.id;
   const msg = messages.find((message) => message.id === id);
   if (msg === undefined) {
     res.status(404).send("message not found");
@@ -57,8 +66,22 @@ app.delete("/messages/:id", (req, res) => {
   const messageIndex = messages.findIndex((m) => m.id === id);
   messages.splice(messageIndex, 1);
   res.send("message deleted");
-  res.send("message deleted");
 });
+
+app.get("/messages/search", (req, res) => {
+  if (req.query.search) {
+    let search = req.query.search.text.toLowerCase();
+    let matched = messages.find((message) =>
+      message.text.toLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send("");
+  }
+}),
+  app.get("messages/latest", (req, res) => {
+    res.json(messages[messages.length - 1]);
+  });
 const port = 3131;
 
 app.listen(port, () => {
